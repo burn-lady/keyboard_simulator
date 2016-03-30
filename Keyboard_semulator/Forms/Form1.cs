f3beb724515b3758bc411b4ef9dafb46f9ecdfcd
burn-lady <burn-lady19@mail.ru>
2016-03-30 23:00:20 +0300

    added a little changes

--- Keyboard_semulator/Forms/Form1.cs
+++ Keyboard_semulator/Forms/Form1.cs
@@ -181,7 +181,7 @@ namespace Keyboard_semulator
             saveResult();
             fixBlock();
           //  MessageBox.Show("Вы закончили упражнение! \n" + DateTime.Now.ToString());
-            new InfoForm(userName + " , Поздравляем! Вы закончили упражнение!  \n"+"               "+ DateTime.Now.ToString()).ShowDialog();
+            new InfoForm( userName + " , Поздравляем! Вы закончили упражнение!  \n" + "               "+ DateTime.Now.ToString()).ShowDialog();
         }
 
         private void saveResult()
--- Keyboard_semulator/Forms/infoForm.Designer.cs
+++ Keyboard_semulator/Forms/infoForm.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Orange;
-            this.label1.Location = new System.Drawing.Point(30, 224);
+            this.label1.Location = new System.Drawing.Point(33, 225);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 26);
             this.label1.TabIndex = 1;
@@ -61,7 +61,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(625, 350);
+            this.ClientSize = new System.Drawing.Size(638, 350);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
